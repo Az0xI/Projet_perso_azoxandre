179b34f75027731e78bcf1a4de6f03cafa4fad1f
Alexandre Kubiaczyk <alexandre.kubiaczyk@epitech.eu>
2025-05-05 00:59:39 +0000

    [upd][GoL] nothing has really change beside no more segfault :/

--- Game_Of_Life/src/game.c
+++ Game_Of_Life/src/game.c
@@ -17,22 +17,19 @@ static void display_map(char **map)
 
 static void process_changes(char **map, int i, int j)
 {
-    int nb_voisin = 0;
-    int x = i - 1;
-    int y = j - 1;
+    int nb_voisin = map[i][j] == 'X' ? -1 : 0;
+    int x = i == 0 ? i : i - 1;
+    int y = j == 0 ? j : j - 1;
 
     // Check for neighbour and error handling
-    for (; x <= i + 1; x++) {
-        if (x < 0) x = 0;
-        if (y < 0) y = 0;
-        for (; y <= j + 1; y++) {
+    for (; x <= i + 1 && map[x] != NULL; x++) {
+        printf("x = %i y = %i\n", x, y);
+        for (; y <= j + 1 && map[x][y] != '\0'; y++) {
             nb_voisin = (map[x][y] != '.' ? nb_voisin + 1 : nb_voisin);
-            printf("mapxy = %c x = %i y = %i\n",map[x][y], x, y);
         }
-        y = j == 0 ? j : j - 1;
+        y = j < 0 ? 0 : j - 1;
     }
-    printf("\n");
-
+    x--;
     // Modify array with the game rules
     if ((nb_voisin < 2 || nb_voisin > 3) && map[x][y] == 'X') map[x][y] = 'O';
     if ((nb_voisin < 2 || nb_voisin > 3) && map[x][y] != 'X') map[x][y] = '.';
@@ -50,8 +47,8 @@ int game_of_life(int ac, char *argv[])
         for (int i = 0; map[i] != NULL; i++)
             for (int j = 0; map[i][j] != '\0'; j++)
                 process_changes(map, i, j);
-        sleep(5);
         display_map(map);
+        sleep(3);
     }
 
     // Free map
