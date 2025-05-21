f2365e1823f635f2b36a8421b4e1f084ce555bd9
Alexandre Kubiaczyk <alexandre.kubiaczyk@epitech.eu>
2025-05-21 13:51:54 +0200

    [feat][RotCub] add comment for better comprehension

--- Ascii_Rotating_Cube/main.rs
+++ Ascii_Rotating_Cube/main.rs
@@ -1,16 +1,18 @@
 // This is the main file of the project
 
+// Inport the lib (called crate in rust) ncurses
 extern crate ncurses;
 
+// Import all ncurses function as it's own (without calling int in a struct)
 use ncurses::*;
 
 // ascii brightness = " .:-=+*#%@"
 const SHADOW: [char; 10] = [' ', '.', ':', '-', '=', '+', '*', '#', '%', '@'];
 
 // main function
-fn main() {
+fn main() -> () {
     initscr();
-    addch(SHADOW[1]);
+    addch(SHADOW[1] as u32);
     refresh();
     getch();
     endwin();
